c461c4703d768a93d4db75435a5d215e2f12b133
foo0x29a <thiago.nobayashi@gmail.com>
2020-08-03 11:36:24 +0900

    Add Palindrome Number

---
+++ easy/palindrome_number.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/palindrome-number/
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        s = str(x)
+        start = 0
+        end = len(s) - 1
+
+        while s[start] == s[end]:
+            if start >= end:
+                return True
+            start += 1
+            end -= 1
+            
+        return False
