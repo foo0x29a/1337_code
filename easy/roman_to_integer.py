b4901bb19ace1f37c0b7621bfc43277b5f13c92e
foo0x29a <thiago.nobayashi@gmail.com>
2020-08-04 08:56:42 +0900

    Add Roman to Integer

---
+++ easy/roman_to_integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        m = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        total = 0
+        for i in range(len(s)-1):
+            c = m[s[i]]
+            n = m[s[i+1]]
+            if n > c:
+                c = -c
+            total += c
+        total += m[s[len(s)-1]]
+        return total
