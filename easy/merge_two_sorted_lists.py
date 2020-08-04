66c3701b343fbf56c0de00fc1bd48618ff6357f9
foo0x29a <thiago.nobayashi@gmail.com>
2020-08-04 10:00:20 +0900

    Add Merge Two Sorted Lists

---
+++ easy/merge_two_sorted_lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ll = ListNode(-1)
+        l = ll
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                l.next = l1
+                l = l1
+                l1 = l1.next
+            else:
+                l.next = l2
+                l = l2
+                l2 = l2.next
+
+        while l1 != None:
+            l.next = l1
+            l = l1
+            l1 = l1.next
+            
+        while l2 != None:
+            l.next = l2
+            l = l2
+            l2 = l2.next
+        
+        return ll.next
+            
