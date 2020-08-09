bfd24daef5ac39e7df434021c06e15deb2f20f3a
foo0x29a <thiago.nobayashi@gmail.com>
2020-08-09 11:02:03 +0900

    Add Length of Last Word

---
+++ easy/length_of_last_word.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/length-of-last-word/submissions/
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip().split(" ")[-1]
+        return len(s)
